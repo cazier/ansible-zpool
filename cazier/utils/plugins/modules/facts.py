61fae538df0f4b95ce1ed766c4245dbccac1c6d3
Brendan Cazier <520246+cazier@users.noreply.github.com>
2023-02-19 03:23:03 +0000

    Preparing for more fact finding

--- cazier/utils/plugins/modules/facts.py
+++ cazier/utils/plugins/modules/facts.py
@@ -4,10 +4,8 @@ import pathlib
 from ansible.module_utils.basic import AnsibleModule  # type: ignore[import]
 
 
-def main() -> None:
-    module = AnsibleModule(argument_spec={}, supports_check_mode=True)
-
-    facts: dict[str, str | bool] = {}
+def homebrew(module: AnsibleModule) -> dict[str, str | bool]:
+    facts: dict[str, str | bool] = {"success": True}
 
     path = module.get_bin_path(
         "brew",
@@ -20,7 +18,7 @@ def main() -> None:
 
     if not path:
         facts["homebrew_installed"] = False
-        module.exit_json(ansible_facts=facts, changed=False)
+        return facts
 
     facts["homebrew_installed"] = True
     facts["homebrew_prefix"] = str(pathlib.Path(path).parents[1])
@@ -28,17 +26,31 @@ def main() -> None:
     rc, stdout, stderr = module.run_command([path, "--version"])  # pylint: disable=invalid-name
 
     if rc != 0:
-        module.fail_json(msg=f"An error occurred executing the brew binary: `{stderr}`")
+        facts.update(success=False, msg=f"An error occurred executing the brew binary: `{stderr}`")
+        return facts
 
     match = re.match(r"^Homebrew (?P<homebrew_version>.*?\d+) .*?\n.*", stdout)
 
     if not match:
-        module.fail_json(msg=f"Could not determine the brew version. Output: `{stdout}")
-        return
+        facts.update(success=False, msg=f"Could not determine the brew version. Output: `{stdout}")
+        return facts
 
     facts.update(match.groupdict())
 
-    module.exit_json(ansible_facts=facts, changed=False)
+    return facts
+
+
+def main() -> None:
+    module = AnsibleModule(argument_spec={}, supports_check_mode=True)
+
+    union: dict[str, str | bool] = {}
+
+    if not (facts := homebrew(module)).pop("success"):
+        module.fail_json(msg=facts.pop("msg"))
+
+    union.update(facts)
+
+    module.exit_json(ansible_facts=union, changed=False)
 
 
 if __name__ == "__main__":
