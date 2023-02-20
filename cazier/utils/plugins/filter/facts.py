bab014807ae285f70c7243a1540a0d21a4aab252
Brendan Cazier <520246+cazier@users.noreply.github.com>
2023-02-20 17:57:10 +0000

    fixup! Adding a filter to get data

--- cazier/utils/plugins/filter/facts.py
+++ cazier/utils/plugins/filter/facts.py
@@ -2,11 +2,11 @@ import typing as t
 
 
 class FilterModule:
-    def filters(self) -> dict[str, t.Callable[[dict[str, dict[str, str]], str], str]]:
+    def filters(self) -> dict[str, t.Callable[[str, dict[str, t.Any]], str]]:
         return {"users_profile": self.users_profile, "users_shell": self.users_shell}
 
-    def users_profile(self, shells: dict[str, dict[str, str]], username: str) -> str:
-        return shells[username]["profile"]
+    def users_profile(self, username: str, ansible_facts: dict[str, t.Any]) -> str:
+        return ansible_facts["user_shells"][username]["profile"]
 
-    def users_shell(self, shells: dict[str, dict[str, str]], username: str) -> str:
-        return shells[username]["shell"]
+    def users_shell(self, username: str, ansible_facts: dict[str, t.Any]) -> str:
+        return ansible_facts["user_shells"][username]["shell"]
