a2c3624d68503ff1fabdda08933bada2cc78e654
Brendan Cazier <520246+cazier@users.noreply.github.com>
2023-02-20 17:16:26 +0000

    Shell facts

--- cazier/utils/plugins/modules/facts.py
+++ cazier/utils/plugins/modules/facts.py
@@ -3,6 +3,8 @@ import pathlib
 
 from ansible.module_utils.basic import AnsibleModule  # type: ignore[import]
 
+SHELL_PROFILES = {"zsh": ".zshrc", "bash": ".bashrc"}
+
 
 def homebrew(module: AnsibleModule) -> dict[str, str | bool]:
     facts: dict[str, str | bool] = {"success": True}
@@ -40,15 +42,41 @@ def homebrew(module: AnsibleModule) -> dict[str, str | bool]:
     return facts
 
 
+def shells() -> dict[str, bool | dict[str, dict[str, str]]]:
+    try:
+        import pwd  # pylint: disable=import-outside-toplevel
+
+    except ImportError:
+        return {"success": True}
+
+    facts: dict[str, dict[str, dict[str, str]]] = {"user_shells": {}}
+
+    for struct in pwd.getpwall():
+        if struct.pw_name.startswith("_"):
+            continue
+
+        facts["user_shells"][struct.pw_name] = {
+            "shell": struct.pw_shell,
+            "profile": SHELL_PROFILES.get(struct.pw_shell.split("/")[-1], ""),
+        }
+
+    return {"success": True, **facts}
+
+
 def main() -> None:
     module = AnsibleModule(argument_spec={}, supports_check_mode=True)
 
-    union: dict[str, str | bool] = {}
+    union: dict[str, str | bool | dict[str, dict[str, str]]] = {}
 
-    if not (facts := homebrew(module)).pop("success"):
-        module.fail_json(msg=facts.pop("msg"))
+    if not (homebrew_facts := homebrew(module)).pop("success"):
+        module.fail_json(msg=homebrew_facts.pop("msg"))
 
-    union.update(facts)
+    union.update(homebrew_facts)
+
+    if not (shell_facts := shells()).pop("success"):
+        module.fail_json(msg=shell_facts.pop("msg"))
+
+    union.update(shell_facts)
 
     module.exit_json(ansible_facts=union, changed=False)
 
